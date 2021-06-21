c944101e682f43dfa1fca8ade0fae81ca3245f2e
nassir <daniya_n@yahoo.com>
2021-06-21 16:16:23 -0700

    Updates

---
+++ precache-manifest.e0e9dd6ae540a058107ddbf98ce418a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a30dda229a49e170a89004146e12798",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "c2b626f41993a5821105",
+    "url": "/todo/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "fbbaccaea09319ff2b79",
+    "url": "/todo/static/css/main.fa9bd362.chunk.css"
+  },
+  {
+    "revision": "c2b626f41993a5821105",
+    "url": "/todo/static/js/2.16347501.chunk.js"
+  },
+  {
+    "revision": "82816ae208d4f9eeb40d7d0faf2ae910",
+    "url": "/todo/static/js/2.16347501.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbbaccaea09319ff2b79",
+    "url": "/todo/static/js/main.251caca8.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
